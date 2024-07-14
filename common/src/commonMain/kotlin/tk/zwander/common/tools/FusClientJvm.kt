e8b568bd7c0c35c8ca740b0157842fd739e6f0c8
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-07-14 09:21:53 +0200

    Update FusClientJvm.kt

--- common/src/commonMain/kotlin/tk/zwander/common/tools/FusClientJvm.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/tools/FusClientJvm.kt
@@ -1,1 +1,12 @@
-#
+import com.soywiz.korio.stream.AsyncInputStream
+import com.soywiz.korio.stream.openAsync
+import java.io.File
+
+actual suspend fun doDownloadFile(client: FusClient, fileName: String, start: Long = 0): Pair<AsyncInputStream, String?> {
+    val file = File(fileName)
+    if (!file.exists()) {
+        file.createNewFile()
+    }
+    val inputStream = file.inputStream().openAsync()
+    return Pair(inputStream, file.absolutePath)
+}
